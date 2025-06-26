e0ce1c7e092a2e48b482922c4f74d867d91016ec
Luiz Souza <108751129+luizinmoc@users.noreply.github.com>
2025-06-26 17:41:08 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import { Link } from "react-router";
+import React from 'react';
+import './App.css';
+
+// Importa o componente que você colocou em src/components
+import Formulario from './components/Formulario';
 
 function App() {
-    return (
-		<>
-			<h1 className="text-5xl text-center font-bold">Método de Newton e Newton Modificado</h1>
-			<h4 className="text-xl text-center">Otimização não linear</h4>
-			<Link
-				to="/parametros"
-				className="flex justify-center w-68 text-2xl bg-gray-800 disabled:bg-zinc-300 cursor-pointer dark:disabled:bg-zinc-600 border-0 focus:bottom-0 hover:border-0 hover:opacity-95 focus:outline-none text-white font-semibold rounded transition-transform transform active:scale-95"
-			>
-				<div>Iniciar Otimização</div>
-			</Link>
-			
-		</>
-	);
+  return (
+    <div className="App">
+      <h1>Otimização Não Linear</h1>
+      <Formulario />
+    </div>
+  );
 }
 
 export default App;
